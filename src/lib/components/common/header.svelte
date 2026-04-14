c0b294b2c449812d461edfa4765a05c8e9ce7789
quilion.oliveira <quilion.oliveira@lsinn.com.br>
2026-04-13 23:24:52 -0300

    feat: initialize SvelteKit project with Tailwind CSS, Vitest, and UI component structure

---
+++ src/lib/components/common/header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ModeToggle from '../mode-toggle.svelte';
+</script>
+
+<header>
+	<title>NuuPlayer</title>
+	<ModeToggle />
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>NuuPlayer</title>
+	<meta name="description" content="Descubra jogos selecionados especialmente para o seu estilo de jogo." />
+</svelte:head>
+
+<div class="min-h-screen flex items-center justify-center p-4">
+	<h1>Teste</h1>
+</div>
